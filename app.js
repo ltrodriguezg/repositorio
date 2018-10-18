3b11d788e2244d879de36166c0d079e559fc39cc
edgarcespedes <ltrodriguezg@unincca.edu.co>
2018-10-18 16:25:06 -0500

    todo funcionando

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require ('./config/config');
 const express = require ('express');
 const bodyparser = require('body-parser');
 const morgan = require ('morgan');
-const session = require ('cookie-session');
+ const session = require ('cookie-session');
 const port = process.env.PORT;
 const app = express();
 
@@ -13,11 +13,39 @@ app.use (bodyparser.urlencoded({
 
 );
 
-// app.use(
-//     session({
-//   secret: 'node'  
-// }));
+   app.use(
+       session({
+   secret: 'node'  
+   }));
+
+ // configuracion de EJS Template Engine
+ app.set('view engine', 'ejs')
+let tareas= ['uno','dos'];
+
+//compartir recursos 
+app.use('/public', express.static('public'))
+
+//ruta iniciada
+ app.get('/',function(request,response){
+     response.render('formulario.ejs',{
+         tareas
+     })
+ })
+
+ //ruta formulario
+app.post('/adicionar',function(request,response){
+    let tarea = request.body.nuevaTarea;
+    tareas.push(tarea);
+    response.redirect('/')
+})
+//borrar tareas
+app.get('/borrar/:id',function(request,response){
+    let id = +request.params.id;
+    tareas.splice(id,1)
+    response.redirect('/');
+    
+})
 
 app.listen(port, function(){
-    console.log('escuhcando el puerto', port);
+    console.log('escuchando el puerto', port);
 });
